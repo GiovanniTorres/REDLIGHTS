76ee5b94c0d2bfa1e7cc34b20832f801b2df9418
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-23 11:03:47 +0000

    1

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../core/Autoload.php';
+require_once __DIR__ . '/../core/Router.php';
+
+use Core\Router;
+
+echo "Cargando index.php<br>";
+
+$router = new Router();
+$router->handleRequest();
