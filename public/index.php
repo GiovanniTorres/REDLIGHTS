1c888a451ba174e1e8fe25de7ab67b1d85d77e8e
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-04-23 05:11:00 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
-require_once __DIR__ . '/../core/Autoload.php';
-require_once __DIR__ . '/../core/Router.php';
+//require_once __DIR__ . '/../core/Autoload.php';
+//require_once __DIR__ . '/../core/Router.php';
 
-use Core\Router;
+//use Core\Router;
 
 echo "Cargando index.php<br>";
 
-$router = new Router();
-$router->handleRequest();
+//$router = new Router();
+//$router->handleRequest();
